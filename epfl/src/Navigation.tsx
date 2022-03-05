4e9e368d67a5ac3ec4b7dac28fe2cb9e984db84c
Gladys <tigerpillow@outlook.com>
2022-03-06 00:01:04 +0100

    changed nav bar

--- epfl/src/Navigation.tsx
+++ epfl/src/Navigation.tsx
@@ -1,40 +1,29 @@
 import React from "react";
+import{Navbar, Container, NavDropdown, Nav} from 'react-bootstrap';
 
 
 function Navigation () {
 
   return(
     <div>
-      
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">EPFL CourseRank</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/form">Submit</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" href="/CS">CS</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" href="/MTE">MTE</a>
-              </li>
-
-            </ul>
-            <form className="d-flex">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
-
-
-      
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">EPFL CourseRank</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/form">Submit Rating</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/cs">IN</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">MTE</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">CS</NavDropdown.Item>
+          
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+  </Navbar>
     </div>
   )
 }
